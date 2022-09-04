15e9acc74b076cab30d9fb0181603dfd74cded12
猫猫 <overflowcat@gmail.com>
2022-09-04 18:27:15 +0800

    add `fake.js`

---
+++ fake.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Zhihu Watermark Faker
+// @namespace    http://overflow.cat/
+// @version      0.1
+// @description  Remove blind watermarks from Zhihu webpages
+// @author       OverflowCat
+// @match        https://www.zhihu.com/*
+// @license      WTFPL
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=zhihu.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    document.querySelectorAll("#root > div > div").forEach(ele => {
+        if (getComputedStyle(ele).backgroundImage.startsWith(`url("data:image/svg+xml;`)) {
+            ele.setAttribute('style',
+                `background-image: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzMzYgNzUiIGZpbGw9IiMwMDAiIGZpbGwtb3BhY2l0eT0iMC4wMDUiIHdpZHRoPSIzMzYiIGhlaWdodD0iNzUiIGZvbnQtc2l6ZT0iMTVweCI+PHRleHQgeD0iMTAiIHk9IjEuNWVtIj4xMTQ1MTQxOTE5ODEwYWFhYWFhYWFhYWFhYTwvdGV4dD48dGV4dCB4PSIxMCIgeT0iM2VtIj4xMTQ1MTQxOTE5ODEwPC90ZXh0Pjwvc3ZnPg==") !important`
+            );
+        }
+    })
+})();
